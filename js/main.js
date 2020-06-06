d762f4dcbb2a164e9b7d9f3408b92bc5f6611647
Amet-Han <velilyaev1999@bk.ru>
2020-06-07 02:24:32 +0300

    test

--- js/main.js
+++ js/main.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
       $('.header__burger,.header__burger-circle,.header__menu').toggleClass('active');
       $('body').toggleClass('lock');
    });
+   $('.btn_submit').click(function(event) {
+   	setTimeout(clrinput, 2000);
+   	function clrinput(){
+   	$('.phone,.name').val("");
+   	}
+   });
+});
+var submitted=false;
+$('#gform').on('submit', function(e) {
+  $('.sended').addClass('active');
 });
